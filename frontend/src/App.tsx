3d279640cf4ecec1a702b3fed8ce92fc049e8960
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-07-02 12:03:03 +0200

    feat<frontend | main_page>: set the basic general layout of the main page of the site

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { Layout } from 'antd';
+import style from './App.module.css';
+import { Content, Header } from 'antd/es/layout/layout';
+import Sider from 'antd/es/layout/Sider';
 
 function App() {
   return (
-    <>
-      Hello World!
-    </>
+    <div className={style["main"]}>
+      <Layout className={style["layoutStyle"]}>
+      <Sider className={style["siderStyle"]} width="25%">
+        Sider
+      </Sider>
+      <Layout>
+        <Header className={style["headerStyle"]}>Header</Header>
+        <Content className={style["contentStyle"]}>Content</Content>
+      </Layout>
+    </Layout>
+    </div>
   )
 }
 
